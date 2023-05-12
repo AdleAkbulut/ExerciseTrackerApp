1f4a3c6c3641f9055d5a259d861a8b7ad15b131a
AdleAkbulut <akbul22a@mtholyoke.edu>
2023-05-12 12:58:59 -0400

    Worked on Context and Hooks

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -24,14 +24,15 @@ const getWorkout = async (req, res) => {
 //create a new workout 
 const createWorkout = async (req, res) => {
     const {title, load, reps} = req.body
-    //add doc to db
-    try{
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
+  
+    // add to the database
+    try {
+      const workout = await Workout.create({ title, load, reps })
+      res.status(200).json(workout)
     } catch (error) {
-        res.status(400).json({error: error.message})
+      res.status(400).json({ error: error.message })
     }
-}
+  }
 
 //delete a workout
 const deleteWorkout = async (req, res) => {
