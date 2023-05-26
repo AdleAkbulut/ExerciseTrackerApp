80d725f5fe44262123ff63b38799fc4f6538b213
AdleAkbulut <akbul22a@mtholyoke.edu>
2023-05-26 14:56:19 -0400

    Sign up validation updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -19,6 +20,17 @@ const userSchema = new Schema({
 userSchema.statics.signup = async function(email, password) {
 
   const exists = await this.findOne({ email })
+
+  //validation
+  if (!email || !password){
+    throw Error("All fields must be filled")
+  }
+  if (!validator.isEmail(email)){
+    throw Error("Email is not valid")
+  }
+  if (!validator.isStrongPassword(password)){
+    throw Error("Password is not strong enough")
+  }
 
   if (exists) {
     throw Error('Email already in use')
