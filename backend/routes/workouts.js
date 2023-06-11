0e4e5f8bbf19acb312798e6770222096fc3326c6
AdleAkbulut <akbul22a@mtholyoke.edu>
2023-06-11 12:09:19 -0400

    Finished authorization

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const {createWorkout, getWorkouts, getWorkout, deleteWorkout,updateWorkout} = require("../controllers/workoutController")
+const requireAuth = require("../middleware/requireAuth")
 
 const router = express.Router()
+
+//Require auth for all workout routes
+router.use(requireAuth)
 
 //Get all workouts
 router.get("/", getWorkouts)
--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -1,10 +1,11 @@
-import { WorkoutsContext } from "../context/WorkoutsContext"
-import { useContext } from "react"
+import React from 'react'
+import { WorkoutsContext } from '../context/WorkoutsContext'
+import { useContext } from 'react'
 
 export const useWorkoutsContext = () => {
   const context = useContext(WorkoutsContext)
 
-  if(!context) {
+  if (!context) {
     throw Error('useWorkoutsContext must be used inside an WorkoutsContextProvider')
   }
 
